dc6e17c2e7bd1480e6d6226cc721fcc29e445ce8
Vladimir Goshev <Vladimir.Goshev@oracle.com>
2017-01-23 13:10:42 +0300

    Cleanup: Delete useless files

--- make-tgz.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh -x
-#Make tar.gz archive of this folder for easier distribution
-
-tar cvzf ../ODS-Dockerfile.tgz --exclude='*.tgz' --exclude='*/*.tar.bz2' ./
--- make-zip.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh -x
-#Make .zip archive of this folder for easier distribution
-
-#tar cvzf ../ODS-Dockerfile.tgz --exclude='*.tgz' --exclude='*/*.tar.bz2' ./
-zip --exclude '*/*.tar.bz2' -r ../ODS-Dockerfile.zip .
